fcfbefc38af1f0f6b93ffae1a4c417be2d39b716
asmirsabanovic <asmir.saban@gmail.com>
2014-11-23 17:21:52 +0100

    added stuff

--- TestApplication/app/src/main/java/com/example/assa/testapplication/MyActivity.java
+++ TestApplication/app/src/main/java/com/example/assa/testapplication/MyActivity.java
@@ -6,6 +6,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -15,7 +16,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,7 +44,10 @@ public class MyActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_my);
+
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -49,6 +57,10 @@ public class MyActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+
     }
 
     @Override
@@ -66,11 +78,11 @@ public class MyActivity extends Activity
 
                 break;
         }
-        Bundle args = new Bundle();
-        args.putStringArrayList("list", listarray);
+        listarray.add("Asmir");
+        listarray.add("Alden");
 
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, PlaceholderFragmentList.newInstance(listarray))
                 .commit();
     }
 
@@ -99,30 +111,31 @@ public class MyActivity extends Activity
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.my, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+//            // Only show items in the action bar relevant to this screen
+//            // if the drawer is not showing. Otherwise, let the drawer
+//            // decide what to show in the action bar.
+//            getMenuInflater().inflate(R.menu.my, menu);
+//
+//            restoreActionBar();
+//            return true;
+//        }
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     /**
      * A placeholder fragment containing a simple view.
@@ -190,10 +203,10 @@ public class MyActivity extends Activity
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragmentList newInstance() {
+        public static PlaceholderFragmentList newInstance(ArrayList<String> thelist) {
             PlaceholderFragmentList fragment = new PlaceholderFragmentList();
             Bundle args = new Bundle();
-           // args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+           args.putStringArrayList("list", thelist);
             fragment.setArguments(args);
             return fragment;
         }
@@ -204,18 +217,22 @@ public class MyActivity extends Activity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            int section = getArguments().getInt(ARG_SECTION_NUMBER);
+            ArrayList<String> testList = getArguments().getStringArrayList("list");
             View rootView = null;
-            switch(section){
-                case 1:  rootView = inflater.inflate(R.layout.fragment_my, container, false);
-                    break;
-                case 2:  rootView = inflater.inflate(R.layout.fragment_my2, container, false);
-                    break;
-                case 3:  rootView = inflater.inflate(R.layout.fragment_my3, container, false);
-                    break;
-                default: rootView = inflater.inflate(R.layout.fragment_my, container, false);
-                    break;
-            }
+
+            rootView = inflater.inflate(R.layout.fragment_webview, container, false);
+            //ListView lv = (ListView)rootView.findViewById(R.id.listView);
+            WebView webview = (WebView) rootView.findViewById(R.id.webView);
+            webview.setWebViewClient(new myWebClient());
+            WebSettings webSettings = webview.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webview.setWebViewClient(new myWebClient());
+            webview.loadUrl("http://www.expressen.se");
+
+
+
+
+
 
             return rootView;
         }
@@ -223,8 +240,26 @@ public class MyActivity extends Activity
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((MyActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+
         }
     }
+
+    public static class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+    }
+
 }
